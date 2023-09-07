250e5ba4211e4a64ed3679411393bd9a0dd8e42f
Og1Kenobi <jon.r.douglas@me.com>
2023-09-07 13:34:22 -0500

    modified:   old_file_purge.py

--- old_file_purge.py
+++ old_file_purge.py
@@ -5,10 +5,9 @@ import datetime
 
 def delete_old_files(directory):
     now = datetime.datetime.now()
-# uncomment one of the follwing or write yor own
     six_months_ago = now - datetime.timedelta(days=6*30)
-  # one_week_ago = now - datetime.timedelta(days=7)
-  # one_year_ago = now - datetime.timedelta(days=365)
+    # one_week_ago = now - datetime.timedelta(days=7)
+    # one_year_ago = now - datetime.timedelta(days=365)
 
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -18,7 +17,8 @@ def delete_old_files(directory):
                 os.remove(file_path)
                 print(f"Deleted file: {file_path}")
 
-# Provide the directory path where you want to delete files older than 6 months
-directory_path = ""
+# Provide the directory path where you want to delete files
+directory_path = "/path/to/directory"
 
 delete_old_files(directory_path)
+
